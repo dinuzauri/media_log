e39541a5393b40e3912bae721559b41648ed8cb8
Dinu Bălan <dinu@ik.me>
2025-03-09 17:54:00 +0200

    ci(settings.py): Added pythonanywhere as allowed host

--- media_log/settings.py
+++ media_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ux$4d#+q=zww*+az7(6#((0d$$*i9rw)b^t&a0!ekm$^&y272a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dinuzauri.pythonanywhere.com"]
 
 
 # Application definition
