debc8dd4b93323207018e8e559ee372d29189787
Dinu Bălan <dinu@ik.me>
2025-05-29 15:23:59 +0300

    ci(backend): removed debug true flag for production

--- media_log/settings.py
+++ media_log/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ux$4d#+q=zww*+az7(6#((0d$$*i9rw)b^t&a0!ekm$^&y272a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dinuzauri.pythonanywhere.com"]
 
